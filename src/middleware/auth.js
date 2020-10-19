c5d8ce26b6e0c7baa5e413aa6723cb70476c02ba
Bartek Mogielnicki <b.mogielnicki@gmail.com>
2020-10-19 17:06:54 +0200

    Prepare to upload to heroku

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,7 +4,7 @@ const User = require('../models/user');
 const auth = async (req, res, next) => {
   try {
     const token = req.headers.authorization.replace('Bearer ', '');
-    const decoded = jwt.verify(token, 'testsecretmessage');
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
     const user = await User.findOne({
       _id: decoded._id,
       'tokens.token': token,
